13b0142640e6315745c9d32e392863f2b9839c65
Martin Marchev <n1tmare@gmail.com>
2017-03-04 11:36:57 +0200

    Initial commit

---
+++ src/test/java/org/marchev/gated/config/GatedConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package org.marchev.gated.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatedConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
